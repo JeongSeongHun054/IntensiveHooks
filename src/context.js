c03f97bbac86cc99fde48067436d706355b53008
JeongSeongHun054 <gnsl1465@naver.com>
2020-09-22 17:36:01 +0900

    useContext Improvements

--- src/context.js
+++ src/context.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 export const UserContext = React.createContext();
 
 // const UserContextProvider = ({ children }) => (
 //   <UserContext.Provider value={{ name: "hun" }}>
-//     {/* 여기서의 children은 다른 Component들을 뜻한다 */}
+
 //     {children}
 //   </UserContext.Provider>
 // );
@@ -16,10 +16,22 @@ const UserContextProvider = ({ children }) => {
   });
   const logUserIn = () => setUser({ ...user, loggedIn: true });
   return (
-    <UserContext.Provider value={{ user, logUserIn }}>
+    //   value에 전역적으로 들어갈 state들을 넣어줌으로써 다른 Component에서 사용가능하게 한다.
+    <UserContext.Provider value={{ user, fn: { logUserIn } }}>
+      {/* 여기서의 children은 다른 Component들을 뜻한다 */}
       {children}
     </UserContext.Provider>
   );
+};
+
+export const useFns = () => {
+  const { fn } = useContext(UserContext);
+  return fn;
+};
+
+export const useUser = () => {
+  const { user } = useContext(UserContext);
+  return user;
 };
 
 export default UserContextProvider;
